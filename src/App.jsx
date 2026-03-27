eca28607b6967aef68f17ac315593a023d645bd7
Melui <virginiadambrosio610@gmail.com>
2026-03-27 18:04:18 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter,Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 
 export default function App() {
     return (
-        <Routes>
+        <HashRouter>
             <Route path="/" element={<Home />} />
             <Route path="/projets" element={<Projects />} />
             <Route path="/about" element={<About />} />
-        </Routes>
+        </HashRouter>
     );
 }
